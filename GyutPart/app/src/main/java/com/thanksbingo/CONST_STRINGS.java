ae00953ced3c0f7a1885f1aa7805d0e26ea65bbd
GyutaeHa <hagyut@gmail.com>
2015-02-13 21:49:54 +0900

    db part

---
+++ GyutPart/app/src/main/java/com/thanksbingo/CONST_STRINGS.java
@@ -0,0 +1,11 @@
+package com.thanksbingo;
+
+/**
+ * Created by Hagyut on 2015. 2. 12..
+ */
+public class CONST_STRINGS {
+
+    public static final String BINGO_LOG = "BINGO LOG";
+    public static final String SP_FILE_KEY = "NEXTERS_BINGO";
+    public static final String SP_FOOD_INFO_LAST_HISTORY = "FOOD INFO LAST HISTORY";
+}
--- GyutPart/app/src/main/java/com/thanksbingo/db/BingoDB.java
+++ GyutPart/app/src/main/java/com/thanksbingo/db/BingoDB.java
@@ -2,12 +2,16 @@ package com.thanksbingo.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.thanksbingo.CONST_STRINGS;
 import com.thanksbingo.httpclient.BingoHttpClient;
 import com.thanksbingo.icondownloader.IconDownloader;
 
@@ -18,7 +22,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class BingoDB {
 
@@ -63,15 +69,11 @@ public class BingoDB {
         public void writeDataToFoodInfoTable(String food_name, int rec_exp_date, String icon_img_path1, String icon_img_path2, long frequency)
         public void writeDataToFoodInfoTable(ArrayList<FoodInfoContract.FIData> data)
 
-    2. FoodInfo Table 과 ExtraFoodInfo Table 에 존재하는 모든 음식물의 리스트(식별자, 이름, 등록 빈도)를 받아올 때, --> for 음성 인식
-        * 이 함수들을 호출할 때, AsyncTask 사용하여야 하는가?
-        public ArrayList<OtherClasses.SomeFood> getListOfWholeFood()
-        public ArrayList<OtherClasses.SomeFood> getListOfWholeFood(int bottom_limit)
-        public ArrayList<OtherClasses.SomeFood> getListOfWholeFood(int bottom_limit, int count_limit)
+    2.
 
      */
 
-    public void loadFoodInfoDataFromServerAndStoreThemOnDB() {
+    public void loadAndStoreFoodInfoStartActivity(final Intent intent) {
 
         BingoHttpClient bh_client = new BingoHttpClient();
         final String subURL = "bingo_api/get_food_info/";
@@ -80,25 +82,41 @@ public class BingoDB {
             public void onSuccess(int resp_code, Header[] headers, byte[] bytes) {
                 try {
                     String foodInfoList_json = new String(bytes, "UTF-8");
-                    IconDownloader iconDownloader = new IconDownloader(null, "/Bingo/Icons/");
+                    IconDownloader icon_downloader = new IconDownloader(null, "/Bingo/Icons/");
 
                     try {
                         JSONObject jObject = new JSONObject(foodInfoList_json);
-                        JSONArray jArray = new JSONArray(jObject.getJSONArray("food_info_list"));
-                        for (int i = 0; i < jArray.length(); i++) {
-                            JSONObject foodInfo = jArray.getJSONObject(i);
-                            FoodInfoContract.FIData data = new FoodInfoContract.FIData();
-                            data.food_id = foodInfo.getInt("food_id");
-                            data.food_name = foodInfo.getString("food_name");
-                            data.rec_exp = foodInfo.getInt("rec_exp");
-                            data.frequency = foodInfo.getInt("frequency");
-                            try {
-                                data.icon_img1 = iconDownloader.downloadIconImage(foodInfo.getString("icon1"));
-                                data.icon_img2 = iconDownloader.downloadIconImage(foodInfo.getString("icon2"));
-                            } catch (IconDownloader.NoIconUrlException e) {
-                                e.printStackTrace();
-                            } catch (IconDownloader.UndecidedIconsDirPath e) {
-                                e.printStackTrace();
+
+                        boolean need_update = jObject.getBoolean("need_update");
+
+                        if (need_update) {
+                            JSONArray jArray = new JSONArray(jObject.getString("food_info_list"));
+
+                            int last_history = jObject.getInt("last_history");
+
+                            String LAST_HISTORY = "LAST HISTORY";
+                            SharedPreferences sharedPref = context.getSharedPreferences(CONST_STRINGS.SP_FILE_KEY, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+
+                            editor.putInt(CONST_STRINGS.SP_FOOD_INFO_LAST_HISTORY, last_history);
+                            editor.commit();
+
+                            for (int i = 0; i < jArray.length(); i++) {
+                                JSONObject food_info = jArray.getJSONObject(i);
+                                FoodInfoContract.FIData data = new FoodInfoContract.FIData();
+                                data.food_id = food_info.getInt("food_id");
+                                data.food_name = food_info.getString("food_name");
+                                data.rec_exp = food_info.getInt("rec_exp");
+                                data.frequency = food_info.getInt("frequency");
+                                try {
+                                    data.icon_img1 = icon_downloader.downloadIconImage(food_info.getString("icon1"));
+                                    data.icon_img2 = icon_downloader.downloadIconImage(food_info.getString("icon2"));
+                                } catch (IconDownloader.NoIconUrlException e) {
+                                    e.printStackTrace();
+                                } catch (IconDownloader.UndecidedIconsDirPath e) {
+                                    e.printStackTrace();
+                                }
+                                writeDataToFoodInfoTable(data);
                             }
                         }
                     } catch (JSONException e) {
@@ -107,105 +125,137 @@ public class BingoDB {
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
+
+                context.startActivity(intent);
             }
 
             @Override
             public void onFailure(int resp_code, Header[] headers, byte[] bytes, Throwable throwable) {
-
+                Log.i(CONST_STRINGS.BINGO_LOG, "loadAndStoreFoodInfoStartActivity method$ response code: " + resp_code);
             }
         });
     }
 
     public void updateFoodInfoDataDB() {
         BingoHttpClient bh_client = new BingoHttpClient();
-
+        
     }
 
+    public void writeDataToFoodInFridgeTable(FoodInFridgeContract.FIFData data) {
+        new SavingFoodInFridgeDataToDB().execute(data);
+    }
+    private class SavingFoodInFridgeDataToDB extends AsyncTask<FoodInFridgeContract.FIFData, Void, Void> {
+        @Override
+        protected Void doInBackground(FoodInFridgeContract.FIFData... fif_arr) {
 
+            db = db_helper.getWritableDatabase();
 
-//    public void writeDataToFoodInfoTable(FoodInfoContract.FIData data) {
-//
-//        new SavingFoodInfoDataToDB().execute(data);
-//    }
-//    public void writeDataToFoodInfoTable(int food_id, String food_name, int rec_exp_date, String icon_img1, String icon_img2, long frequency) {
-//
-//        FoodInfoContract.FIData data = new FoodInfoContract.FIData();
-//        data.food_id = food_id;
-//        data.food_name = food_name;
-//        data.rec_exp = rec_exp_date;
-//        data.icon_img1 = icon_img1;
-//        data.icon_img2 = icon_img2;
-//        data.frequency = frequency;
-//        new SavingFoodInfoDataToDB().execute(data);
-//    }
-//    private class SavingFoodInfoDataToDB extends AsyncTask<FoodInfoContract.FIData, Void, Void> {
-//        protected Void doInBackground(FoodInfoContract.FIData... food_info) {
-//
-//            db = db_helper.getWritableDatabase();
-//
-//            int count = food_info.length;
-//            for (int i = 0; i < count; i++) {
-//
-//                ContentValues values = new ContentValues();
-//                values.put(FoodInfoContract.FoodInfo._ID, food_info[i].food_id);
-//                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FOOD_NAME, food_info[i].food_name);
-//                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_REC_EXP, food_info[i].rec_exp);
-//                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG1, food_info[i].icon_img1);
-//                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG2, food_info[i].icon_img2);
-//                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FREQUENCY, food_info[i].frequency);
-////                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
-////                values.put(FoodListContract.FoodEntry.COLUMN_NAME_UPDATE_DATE, dateFormat.format(food_data[i].date));
-//
-//                db.insert(
-//                        FoodInfoContract.FoodInfo.TABLE_NAME,
-//                        "null",
-//                        values
-//                );
-//            }
-//
-//            return null;
-//        }
-//
-//        protected void onPostExecute(Void v) {
-//        }
-//    }
-//
-//    public void writeDataToFoodInfoTable(ArrayList<FoodInfoContract.FIData> data) {
-//
-//        new SavingFoodInfoArrayDataToDB().execute(data);
-//    }
-//    private class SavingFoodInfoArrayDataToDB extends AsyncTask<ArrayList<FoodInfoContract.FIData>, Void, Void> {
-//        protected Void doInBackground(ArrayList<FoodInfoContract.FIData>... food_info_list) {
-//
-//            db = db_helper.getWritableDatabase();
-//
-//            int count = food_info_list.length;
-//            // food_info[i] -->> ArrayList
-//            for (int i = 0; i < count; i++) {
-//                for (int j = 0; j < food_info_list[i].size(); j++) {
-//                    ContentValues values = new ContentValues();
-//                    FoodInfoContract.FIData food_info = food_info_list[i].get(j);
-//                    values.put(FoodInfoContract.FoodInfo._ID, food_info.food_id);
-//                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FOOD_NAME, food_info.food_name);
-//                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_REC_EXP, food_info.rec_exp);
-//                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG1, food_info.icon_img1);
-//                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG2, food_info.icon_img2);
-//                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FREQUENCY, food_info.frequency);
-//
-//                    db.insert(
-//                            FoodInfoContract.FoodInfo.TABLE_NAME,
-//                            "null",
-//                            values
-//                    );
-//                }
-//            }
-//
-//            return null;
-//        }
-//
-//        protected void onPostExecute(Void v) {
-//        }
-//    }
+            int param_cnt = fif_arr.length;
+            for (int i = 0; i < param_cnt; i++) {
+
+                FoodInFridgeContract.FIFData fif = fif_arr[i];
+
+                ContentValues values = new ContentValues();
+                values.put(FoodInFridgeContract.FoodInFridge._ID, fif.food_id);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+                values.put(FoodInFridgeContract.FoodInFridge.COLUMN_NAME_REG_DATE, dateFormat.format(fif.reg_date));
+                values.put(FoodInFridgeContract.FoodInFridge.COLUMN_NAME_EXP_DATE, dateFormat.format(fif.exp_date));
+                values.put(FoodInFridgeContract.FoodInFridge.COLUMN_NAME_AMOUNT, fif.amount);
+                values.put(FoodInFridgeContract.FoodInFridge.COLUMN_NAME_POSITION, fif.position);
+                values.put(FoodInFridgeContract.FoodInFridge.COLUMN_NAME_HISTORY, fif.history);
+                db.insert(
+                        FoodInFridgeContract.FoodInFridge.TABLE_NAME,
+                        "null",
+                        values
+                );
+            }
+            return null;
+        }
+    }
+
+    public void writeDataToFoodInfoTable(FoodInfoContract.FIData data) {
+
+        new SavingFoodInfoDataToDB().execute(data);
+    }
+    public void writeDataToFoodInfoTable(int food_id, String food_name, int rec_exp_date, String icon_img1, String icon_img2, long frequency) {
+
+        FoodInfoContract.FIData data = new FoodInfoContract.FIData();
+        data.food_id = food_id;
+        data.food_name = food_name;
+        data.rec_exp = rec_exp_date;
+        data.icon_img1 = icon_img1;
+        data.icon_img2 = icon_img2;
+        data.frequency = frequency;
+        new SavingFoodInfoDataToDB().execute(data);
+    }
+    private class SavingFoodInfoDataToDB extends AsyncTask<FoodInfoContract.FIData, Void, Void> {
+        protected Void doInBackground(FoodInfoContract.FIData... food_info) {
+
+            db = db_helper.getWritableDatabase();
+
+            int count = food_info.length;
+            for (int i = 0; i < count; i++) {
+
+                ContentValues values = new ContentValues();
+                values.put(FoodInfoContract.FoodInfo._ID, food_info[i].food_id);
+                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FOOD_NAME, food_info[i].food_name);
+                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_REC_EXP, food_info[i].rec_exp);
+                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG1, food_info[i].icon_img1);
+                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG2, food_info[i].icon_img2);
+                values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FREQUENCY, food_info[i].frequency);
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+//                values.put(FoodListContract.FoodEntry.COLUMN_NAME_UPDATE_DATE, dateFormat.format(food_data[i].date));
+
+                db.insert(
+                        FoodInfoContract.FoodInfo.TABLE_NAME,
+                        "null",
+                        values
+                );
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+        }
+    }
+
+    public void writeDataToFoodInfoTable(ArrayList<FoodInfoContract.FIData> data) {
+
+        new SavingFoodInfoArrayDataToDB().execute(data);
+    }
+    private class SavingFoodInfoArrayDataToDB extends AsyncTask<ArrayList<FoodInfoContract.FIData>, Void, Void> {
+        protected Void doInBackground(ArrayList<FoodInfoContract.FIData>... food_info_list) {
+
+            db = db_helper.getWritableDatabase();
+
+            int count = food_info_list.length;
+            // food_info[i] -->> ArrayList
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < food_info_list[i].size(); j++) {
+                    ContentValues values = new ContentValues();
+                    FoodInfoContract.FIData food_info = food_info_list[i].get(j);
+                    values.put(FoodInfoContract.FoodInfo._ID, food_info.food_id);
+                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FOOD_NAME, food_info.food_name);
+                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_REC_EXP, food_info.rec_exp);
+                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG1, food_info.icon_img1);
+                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_ICON_IMG2, food_info.icon_img2);
+                    values.put(FoodInfoContract.FoodInfo.COLUMN_NAME_FREQUENCY, food_info.frequency);
+
+                    db.insert(
+                            FoodInfoContract.FoodInfo.TABLE_NAME,
+                            "null",
+                            values
+                    );
+                }
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void v) {
+        }
+    }
 
     public void getFoodList(OnGetFoodListHandler handler) {
         new GetFoodList().execute(handler);
